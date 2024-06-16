f5c6142264e8f5984ebe3586c76c1d2febc40313
Rama Astra <ramaastra333@gmail.com>
2024-06-16 23:55:24 +0800

    feat: init main theme config in Tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,16 @@
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {}
+    extend: {
+      fontFamily: {
+        sans: ['Lato', 'sans-serif'],
+        philosopher: ['Philosopher', 'sans-serif']
+      },
+      colors: {
+        primary: '#BD946D',
+        secondary: '#2C3831'
+      }
+    }
   },
   plugins: []
 };
