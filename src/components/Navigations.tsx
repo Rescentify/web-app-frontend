2674cb6b0a9af9c1ba284fe032f7d66c1db7bb19
Rama Astra <ramaastra333@gmail.com>
2024-06-17 22:32:43 +0800

    feat: add active navigation style and add navigation links to other pages

--- src/components/Navigations.tsx
+++ src/components/Navigations.tsx
@@ -2,15 +2,24 @@ import { NavLink } from 'react-router-dom';
 
 const links = [
   { href: '/', name: 'Beranda' },
-  { href: '#', name: 'Katalog' },
-  { href: '#', name: 'Tentang Kami' }
+  { href: '/catalogue', name: 'Katalog' },
+  { href: '/about', name: 'Tentang Kami' }
 ];
 
 function Navigation() {
   return (
     <>
       {links.map((link) => (
-        <NavLink to={link.href}>{link.name}</NavLink>
+        <NavLink
+          to={link.href}
+          className={({ isActive }) =>
+            isActive
+              ? 'relative after:content-[""] after:absolute after:h-2 after:w-4/5 after:border-b-2 after:border-primary after:-bottom-1 after:right-0 font-bold'
+              : 'opacity-90'
+          }
+        >
+          {link.name}
+        </NavLink>
       ))}
     </>
   );
